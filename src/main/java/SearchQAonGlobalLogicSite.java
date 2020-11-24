85e66bd4c2b90ad2ce633d733b6184b6d7180071
arsen.pashko <arsen.pashko@gmail.com>
2020-11-24 14:19:01 +0200

    code editing and add annotations of JUnit

--- src/main/java/SearchQAonGlobalLogicSite.java
+++ src/main/java/SearchQAonGlobalLogicSite.java
@@ -1,3 +1,6 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -7,23 +10,24 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-
-
 public class SearchQAonGlobalLogicSite {
-    public static void main(String[] args) {
+
+    private static WebDriver driver;
+    public WebDriverWait wait = new WebDriverWait(driver, 10);
+    @BeforeClass
+    public static void setup(){
         System.setProperty("webdriver.chrome.driver", "/home/arsen/IdeaProjects/drivers/chromedriver");
-
-        WebDriver driver = new ChromeDriver();
-
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-
+        driver = new ChromeDriver();
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
         driver.get("https://www.globallogic.com/ua/careers/");
         js.executeScript("window.scrollBy(0,200)");
 
         driver.manage().window().maximize();
+    }
 
+    @Test
+    public void getOfVacancyTest(){
         WebElement webElement = driver.findElement(By.id("by_keyword"));
         webElement.sendKeys("QA");
 
@@ -32,8 +36,10 @@ public class SearchQAonGlobalLogicSite {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mb-0")));
         System.out.println(driver.findElement(By.className("mb-0")).getText());
+    }
 
-
-
+    @AfterClass
+    public static void quit(){
+        driver.quit();
     }
 }
